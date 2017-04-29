804ca3dc637dd8bc44e6871f551957f26795fe97
Karns <pkarns@deloitte.com>
2017-04-28 20:38:34 -0400

    updated routes and models

--- models/willslacks.js
+++ models/willslacks.js
@@ -1,24 +1,48 @@
 module.exports = function(sequelize, DataTypes) {
-  var Slacks = sequelize.define("Willmessages", {
+  var Slacks = sequelize.define("way", {
     text: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       // AllowNull is a flag that restricts a todo from being entered if it doesn't
       // have a text value
       allowNull: false,
       // len is a validation that checks that our todo is between 1 and 140 characters
-      validate: {
-        len: [1, 140]
-      }
     },
     timestamp: {
-      type: DataTypes.BOOLEAN,
+      type: DataTypes.DATE,
       // defaultValue is a flag that defaults a new todos complete value to false if
       // it isn't supplied one
       defaultValue: false
     },
     channel: {
-      
+      type: DataTypes.STRING,
+      allowNull: false, 
     }
   });
-  return Willmessages;
+  return "way";
+
+  var Comments = sequelize.define("comment", {
+    username: {
+    type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a comment from being entered if it doesn't
+      // have a text value
+      allowNull: false
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    message_id: {
+      type: DataTypes.INTEGER,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false
+    },
+    text: {
+    type: DataTypes.TEXT,
+      // AllowNull is a flag that restricts a comment from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+    }
+  });
+  return "comment";
 };
