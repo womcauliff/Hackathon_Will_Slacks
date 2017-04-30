97c5d12fde878df47c8d65636f91b4f2cfda3256
Will McAuliff <womcauliff@gmail.com>
2017-04-30 00:06:58 -0400

    Adds error handling and additional console statements.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,9 @@ module.exports = function(app) {
 
   //POST route for slack events
   app.post("/api/slack", function(req, res) {
+
   	console.log(req.body);
+
   	//checks verification token to confirm message is from Slack
   	if(req.body.token !== process.env.SLACK_TOKEN) {
   		console.log("Token mismatch: " + req.body.token);
@@ -22,15 +24,24 @@ module.exports = function(app) {
   	}
 
     //checks that a message was written by the target user
-    if(req.body.event.type == "message" &&
+	  if(req.body.event.type == "message" &&
     req.body.event.user == process.env.TARGET_ID) {
-    	db.Message.create({
-    		text: req.body.event.text,
-    		channel: req.body.event.channel,
-    		ts: req.body.event.ts
-    	}).then(function(dbMessage) {
-    		console.log("added: " + JSON.stringify(dbMessage));
-    	});
+			db.Message.create({
+				text: req.body.event.text,
+				channel: req.body.event.channel,
+				ts: req.body.event_time
+			}).then(function(dbMessage) {
+				console.log("added: " + JSON.stringify(dbMessage));
+				res.status(200).send("ok");
+			}).catch(function(err) {
+				console.log(err);
+				res.status(500).send(err);
+			});
+    }
+    //this message will not be added
+    else {
+			console.log("not adding this message");
+			res.status(200).send("ok");
     }
   });
 };
